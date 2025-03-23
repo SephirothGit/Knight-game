238011506517c32f4d4f1955d16c150b5dde536f
Alex <7276766@gmail.com>
2025-03-23 03:08:07 +0300

    4.Fix bugs, add error handlings

--- Knight-game.go
+++ Knight-game.go
@@ -37,7 +37,7 @@ type Priest struct {
 }
 
 //Fight method
-func (c *BaseCharacter) Fight() error {  //!!!Error handling!!!
+func (c *BaseCharacter) Fight() error { //!!!Error handling!!!
 	fmt.Printf("%s fights\n", c.Name)
 	return nil
 }
@@ -53,38 +53,38 @@ func (c *BaseCharacter) Drink() {
 }
 
 //Eat method
-func (c BaseCharacter) Eat() {
+func (c *BaseCharacter) Eat() {
 	fmt.Printf("%s eat\n", c.Name)
 }
 
 //Fight overridden method for Warrior
 func (k *Knight) Fight() {
-	fmt.Printf("%s (Knight) hit the enemy with a stick", k.Name)
+	fmt.Printf("%s (Knight) hit the enemy with a stick\n", k.Name)
 }
 
 //Fight overridden method for Paladin
 func (p *Paladin) Fight() {
-	fmt.Printf("%s (Paladin) hit the enemy with a holy sword", p.Name)
+	fmt.Printf("%s (Paladin) hit the enemy with a holy sword\n", p.Name)
 }
 
 //Fight overridden method for Mage
 func (m *Mage) Fight() {
-	fmt.Printf("%s (Mage) cast a fireball", m.Name)
+	fmt.Printf("%s (Mage) cast a fireball\n", m.Name)
 }
 
 func (r *Priest) Fight() {
-	fmt.Printf("%s (Priest) use the holy hammer", r.Name)
+	fmt.Printf("%s (Priest) use the holy hammer\n", r.Name)
 }
 
 //Choose location func
 func (c *BaseCharacter) ChooseLocation() {
 	var location string
-	
-	fmt.Print("Where do you want to go? (Forest/Town/Tavern)")          //!!!Fix error handling
-	//Error handling
-//	_, err := fmt.Scanln(&location)
-//	if err != nil {
-//		return fmt.Errorf("input error: %v", err)
+
+	fmt.Print("Where do you want to go? (Forest/Town/Tavern)")
+	_, err := fmt.Scan(&location)
+	if err != nil {
+		fmt.Println("input error", err)
+		return
 	}
 
 	switch location {
@@ -106,72 +106,77 @@ func (c *BaseCharacter) ChooseLocation() {
 }
 
 //Method for going to the forest
-func (c BaseCharacter) GoToForest() {
+func (c *BaseCharacter) GoToForest() {
 	fmt.Printf("%s goes to the Forbidden forest...\n", c.Name)
-	fmt.Println("The monster attacked you!")
+	fmt.Print("The monster has attacked you! ")
 
 	c.Fight() //!!!Make fight method better and more interesting
 }
 
 //Method for going to the town
-func (c BaseCharacter) GoToTown() {
+func (c *BaseCharacter) GoToTown() {
 	fmt.Printf("%s goes to the Town...\n", c.Name) //!!!Upgrade inn and tavern inside town, room for 1, 7, 30 nights, improve dungeon
 
 	//Choose a place to visit in town
 	var choosePlace string
-	fmt.Scan(&choosePlace)
 
 	fmt.Print("Where do you want to go? (dungeon/inn/tavern)")
+	fmt.Scan(&choosePlace)
 
 	switch choosePlace {
 	case "dungeon":
 		fmt.Printf("%s goes to the dungeon...", c.Name)
+
 	case "inn":
-		fmt.Println("%s goes to the Mermaid's inn...", c.Name)
+		fmt.Printf("%s goes to the Mermaid's inn...", c.Name)
 	case "tavern":
-		fmt.Println("%s goes to the Brick's tavern...", c.Name)
+		fmt.Printf("%s goes to the Brick's tavern...", c.Name)
 	default:
 		fmt.Print("Unknown location. Choose from familiar places")
 	}
 
-	var action string
-	fmt.Scan(&action)
+	var actionInside string
+	fmt.Scan(&actionInside)
 
 	fmt.Print("What do you want to do? (Drink/Sleep)")
 
-	switch action {
+	switch actionInside {
 	case "drink":
 		c.Drink()
 	case "sleep":
 		c.Sleep()
 	default:
-		fmt.Println("Unknown action. Please try again")
+		fmt.Print("Unknown action. Please try again")
 	}
 }
 
 //Method for going to the tavern
-func (c BaseCharacter) GoToTavern() {
+func (c *BaseCharacter) GoToTavern() {
 	fmt.Printf("%s goes to the Olaf's tavern...\n", c.Name) //!!Make the choices more difficult, add different rooms, food and drinks
 
 	var tavernAction string
-	fmt.Scan(&tavernAction)
 
-	fmt.Print("Olaf: Hey, traveler! Do you want to drink a cold beer or a room for a night? (Eat/Drink/Sleep)")
+	fmt.Print("Olaf: Hey, traveler! Do you want to drink a cold beer or a room for a night?\n (Eat/Drink/Sleep)")  //!!!Try \n in thi place
+
+	//Error handling
+	_, err := fmt.Scan(&tavernAction)
+	if err != nil {
+		fmt.Println("input error:", err)
+	}
 
 	switch tavernAction {
 	case "drink":
-		fmt.Printf("Olaf: Here's your beer %s", c.Name)
+		fmt.Printf("Olaf: Here's your beer %s\n", c.Name)
 		c.Drink()
 	case "sleep":
 		fmt.Print("Olaf: your room is second on the right side of second floor")
 		c.Sleep()
 	case "eat":
-		fmt.Print("Waitress: Here's your food")
+		fmt.Print("Waitress: Here's your food ")
 		c.Eat()
 	default:
-		fmt.Println("Olaf: We don't offer this option")
+		fmt.Print("Olaf: We don't offer this option")
 	}
-
 }
 
 func StartGame() {
@@ -179,10 +184,19 @@ func StartGame() {
 
 	//Type a name of your character
 	fmt.Print("Type name of your character: ")
-	fmt.Scanln(&name)
+	_, err := fmt.Scanln(&name)
+	if err != nil {
+		fmt.Println("input error:", err)
+		return
+	}
+
 	//Choose a class of character
 	fmt.Print("Choose class of your character: (knight/paladin/mage/priest)")
-	fmt.Scan(&characterClass)
+	_, err = fmt.Scan(&characterClass)
+	if err != nil {
+		fmt.Println("input error:", err)
+		return
+	}
 
 	var character Character
 
@@ -197,6 +211,7 @@ func StartGame() {
 		character = &Priest{BaseCharacter{Name: name}}
 	default:
 		fmt.Println("Unknown class. Create knight by default")
+		character = &Knight{BaseCharacter{Name: name}}
 	}
 	character.ChooseLocation()
 }
